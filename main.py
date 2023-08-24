0c86d4018ddd37706e9340fc7691b83ccc39c1ce
Kirillar2 <92402468+Kirillar2@users.noreply.github.com>
2023-08-24 17:31:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sys
 import os
 
+from moduleReplicateApi import ReplicateInterface
+
 from PyQt5 import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
@@ -17,35 +19,91 @@ class Window(QMainWindow):
         generate = QPushButton("Generate", self)
         generate.move(round(width*0.5-100),round(height*0.5-100))
         generate.resize(200, 100)
+        generate.clicked.connect(self.save_paint)
+
+        clear = QPushButton("Clear", self)
+        clear.move(round(width*0.5-100),round(height*0.5-250))
+        clear.resize(200, 100)
+        clear.clicked.connect(self.clear_paint)
 
         combobox = QComboBox(self)
-        combobox.addItem("Realisitic")
-        combobox.addItem("Realisitic")
-        combobox.addItem("Realisitic")
-        combobox.addItem("Realisitic")
-        combobox.addItem("Realisitic")
+        combobox.addItem("Realism")
+        combobox.addItem("Cubism")
+        combobox.addItem("Oil")
+        combobox.addItem("Watercolor")
+        combobox.addItem("Color pencil")
+        combobox.addItem("Black pencil")
+        combobox.addItem("Charcoal")
+        combobox.addItem("Kashtanov")
+        combobox.addItem("Ivanenko")
+        combobox.addItem("Embroidery")
+        combobox.addItem("Anime")
 
         combobox.move(round(width*0.5-100), round(height*0.5+50))
         combobox.resize(200, 50)
 
         pixmap = QPixmap(768,1024)
         pixmap.fill(Qt.red)
-        label_gen = QLabel(self)
-        label_gen.setPixmap(pixmap)
-        label_gen.move(round(width*0.57),round(height*0.1))
-        label_gen.resize(768,1024)
+        self.label_gen = QLabel(self)
+        self.label_gen.setPixmap(pixmap)
+        self.label_gen.move(round(width*0.57),round(height*0.1))
+        self.label_gen.resize(768,1024)
 
-        canvas = QPixmap(768, 1024)
-        canvas.fill(Qt.white)
-        label_paint = QLabel(self)
-        label_paint.setPixmap(canvas)
-        label_paint.move(round(width*0.1),round(height*0.1))
-        label_paint.resize(768,1024)
+        self.canvas = QPixmap(QSize(768, 1024))
+        self.canvas.fill(Qt.white)
+        self.label_paint = QLabel(self)
+        self.label_paint.move(round(width*0.1),round(height*0.1))
+        self.label_paint.setPixmap(self.canvas)
+        self.label_paint.resize(768,1024)
 
+        self.last_x, self.last_y = None, None
 
+    def mouseMoveEvent(self, e):
+        mouse_x = e.x()-round(width*0.1)
+        mouse_y = e.y()-round(height*0.1)
+        if self.last_x is None:
+            self.last_x = mouse_x
+            self.last_y = mouse_y
+            return
+            
+        painter = QPainter(self.label_paint.pixmap())
+        p = painter.pen()
+        p.setWidth(16)
+        painter.setPen(p)
+        painter.drawLine(self.last_x, self.last_y, mouse_x, mouse_y)
+        painter.drawPoint
+        painter.end()
+        self.update()
 
+        self.last_x = mouse_x
+        self.last_y =  mouse_y
 
-  
+    def mouseReleaseEvent(self, e):
+        self.last_x = None
+        self.last_y = None
+    
+    def save_paint(self):
+        self.label_paint.move(0,0)
+        self.render(self.canvas)
+        self.canvas.save("temp.png", "PNG")
+        self.label_paint.move(round(width*0.1),round(height*0.1))
+        
+        self.genImage()
+
+    def genImage(self):
+        gen = ReplicateInterface("r8_7LQDYseC2kXuZYlhqpIEp3Kt7V4UXLH3M0Lxp")
+        newimg = gen.imageInpaiting("realistic, forest, Russia", "temp.png")
+        gen_paint = QPixmap(newimg)
+        gen_paint = gen_paint.scaled(768,1024)
+        self.label_gen.setPixmap(gen_paint)
+
+    def clear_paint(self):
+        painter = QPainter(self.label_paint.pixmap())
+        p = painter.pen()
+        p.setColor(Qt.white)
+        painter.fillRect(0,0,768,1024, Qt.white)
+        painter.end()
+        self.update()
 
 app = QApplication(sys.argv)
 desktop = QApplication.desktop()
